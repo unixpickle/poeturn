d06a1898387f3a84f6af835952fd465bd2c97ae4
Alex Nichol <unixpickle@gmail.com>
2016-11-30 18:21:55 -0500

    quick & dirty tool to compose a poem

---
+++ compose/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/unixpickle/poeturn/model"
+)
+
+func main() {
+	var netStarts bool
+	var netFile string
+	flag.BoolVar(&netStarts, "netstart", false, "the network starts")
+	flag.StringVar(&netFile, "network", "", "path to the network")
+
+	flag.Parse()
+	if netFile == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	net, err := model.LoadModel(netFile)
+	if err != nil {
+		die("Load model:", err)
+	}
+
+	sess := model.NewSession(net)
+
+	if netStarts {
+		fmt.Println(sess.Query())
+	}
+	for {
+		sess.Dictate(readLine())
+		fmt.Println(sess.Query())
+	}
+}
+
+func readLine() string {
+	fmt.Print("> ")
+	var res string
+	for {
+		var b [1]byte
+		if n, err := os.Stdin.Read(b[:]); err != nil {
+			panic(err)
+		} else if n == 1 {
+			if b[0] == '\n' {
+				break
+			} else if b[0] != '\r' {
+				res += string(b[:])
+			}
+		}
+	}
+	return res
+}
+
+func die(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+	os.Exit(1)
+}
